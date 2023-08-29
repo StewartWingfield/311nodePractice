9ad7fe3c096559a1ad5cc4d5f39e2cc35f721ae8
Stewart Wingfield <sww915@gmail.com>
2023-08-29 17:57:01 -0400

    should be working?

--- src/numbers/index.js
+++ src/numbers/index.js
@@ -1,16 +1,33 @@
 const isEven = (num) => {
   // write code for numbers.isEven
-
+  if (num % 2 === 0) {
+    return true
+  } else {
+    return false
+  }
 }
 
 const sum = (arr) => {
   // write code for numbers.sum
-
+  const initialValue = 0;
+const sumWithInitial = arr.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue)
+return sumWithInitial
 }
 
 const comboSum = (arr, sum) => {
   // write code for numbers.comboSum
+  if (arr.length === 0) {
+    return false;
+  }
 
+  for (let i = 0; i < arr.length; i++) {
+    const num = sum - arr[i]
+    if (arr.indexOf(num, i + 1) !== -1) {
+      return true;
+    } else {
+      return false
+    }
+  }
 }
 
 module.exports = {
--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,27 @@
 const split = (str, delim) => {
   // write code for strings.split
-
+  const arrayOfStrings = str.split(delim)
+  return arrayOfStrings
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
-
+  const pairs = [];
+  
+  for (let i = 0; i < str.length; i += 2) {
+    const pair = str.slice(i, i + 2);
+    pairs.push(pair);
+  }
+  
+  return pairs;
 }
+
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  const reversedArray = str.split('').reverse();
+  const reversedString = reversedArray.join('');
+  return reversedString;
 }
 
 module.exports = {
